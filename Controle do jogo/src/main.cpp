2e94faea50a59f38ace0f0cdb01e457ac1f5e643
GUILHERME RIBEIRO DOS SANTOS <46336142825@info.local>
2025-06-11 09:19:32 -0300

    Criancao-serial-selecao

--- Controle do jogo/src/main.cpp
+++ Controle do jogo/src/main.cpp
@@ -1,12 +1,16 @@
-# include <Arduino.h>
+#include <Arduino.h>
+#include <LiquidCrystal_I2C.h>
 
-# define PIN_A 4
-# define PIN_B 2
-# define PIN_C 12
-# define PIN_D 13
-# define PIN_E 25
+LiquidCrystal_I2C lcd(0x27, 20, 4);
+
+#define PIN_A 2
+#define PIN_B 4
+#define PIN_C 18
+#define PIN_D 5
+#define PIN_E 25
 
 void leituraBotoes(bool leitura[5]);
+int selecaoSerial(bool botaoA, bool botaoB, bool botaoC, bool botaoD, bool botaoE);
 
 void setup()
 {
@@ -15,148 +19,206 @@ void setup()
   pinMode(PIN_C, INPUT_PULLUP);
   pinMode(PIN_D, INPUT_PULLUP);
   pinMode(PIN_E, INPUT_PULLUP);
+
   Serial.begin(9600);
+
+  lcd.init();
+  lcd.backlight();
+  lcd.home();
+  lcd.print(">");
 }
 
 void loop()
 {
   bool botao[5];
+  static int golpes;
   leituraBotoes(botao);
 
-  if(botao[0] == 1)
-  Serial.println("A");
-  if(botao[1] == 1)
-  Serial.println("B");
-  if(botao[2] == 1)
-  Serial.println("C");
-  if(botao[3] == 1)
-  Serial.println("D");
-  if(botao[4] == 1)
-  Serial.println("E");
+  bool A = botao[0];  if(A) Serial.println("A");
+  bool B = botao[1];  if(B) Serial.println("B");
+  bool C = botao[2];  if(C) Serial.println("C");
+  bool D = botao[3];  if(D) Serial.println("D");
+  bool E = botao[4];  if(E) Serial.println("E");
+  golpes = selecaoSerial(A, B, C, D, E);
+}
 
+int selecaoSerial(bool botaoA, bool botaoB, bool botaoC, bool botaoD, bool botaoE)
+{
+  //? Selecao
+  static int selecao;
+  static int selecaoAnterior;
+
+  if (botaoA)
+    selecao--;
+  if (botaoC)
+    selecao++;
+Serial.println(selecao);
+  if (selecao > 3)
+    selecao = 0;
+  if (selecao < 0)
+    selecao = 3;
+
+    //* Impedir que pule 3 a 1
+    if(selecao == 3 && selecaoAnterior == 1)
+    selecao = 2;
+    
+    selecaoAnterior = selecao;
+
+  if (selecao == 0)
+  {
+    lcd.home();
+    lcd.print(">");
+    lcd.setCursor(0, 3);
+    lcd.print(" ");
+    lcd.setCursor(0, 1);
+    lcd.print(" ");
+  }
+  if (selecao == 1)
+  {
+    lcd.setCursor(0, 1);
+    lcd.print(">");
+    lcd.setCursor(0, 0);
+    lcd.print(" ");
+    lcd.setCursor(0, 2);
+    lcd.print(" ");
+  }
+  if (selecao == 2)
+  {
+    lcd.setCursor(0, 2);
+    lcd.print(">");
+    lcd.setCursor(0, 1);
+    lcd.print(" ");
+    lcd.setCursor(0, 3);
+    lcd.print(" ");
+  }
+  if (selecao == 3)
+  {
+    lcd.setCursor(0, 3);
+    lcd.print(">");
+    lcd.setCursor(0, 2);
+    lcd.print(" ");
+    lcd.setCursor(0, 0);
+    lcd.print(" ");
+  }
+  return selecao;
 }
 
 void leituraBotoes(bool leitura[5])
 {
   unsigned int tempo = millis();
-static bool verdadeiro = 1;
+  static bool verdadeiro = 1;
 
-if( tempo > 100)
-{
-  //? Botão A
-  bool estadoA = digitalRead(PIN_A);
-  static bool estadoAnteriorA = 1;
-  static unsigned int tempoAnteriorA;
-  static bool acaoA =1;
-
-  if (estadoA != estadoAnteriorA && estadoA == 0)
-    tempoAnteriorA = tempo;
-  if (tempo - tempoAnteriorA > 100)
+  if (tempo > 100)
   {
-    //Serial.println(acaoD);
-    if (acaoA != estadoA)
+    //? Botão A
+    bool estadoA = digitalRead(PIN_A);
+    static bool estadoAnteriorA = 1;
+    static unsigned int tempoAnteriorA;
+    static bool acaoA = 1;
+
+    if (estadoA != estadoAnteriorA && estadoA == 0)
+      tempoAnteriorA = tempo;
+    if (tempo - tempoAnteriorA > 100)
     {
-    if(estadoA == 0)
-    leitura[0] = 1;
+      if (acaoA != estadoA)
+      {
+        if (estadoA == 0)
+          leitura[0] = 1;
+      }
+      else
+      {
+        leitura[0] = 0;
+      }
+      acaoA = estadoA;
     }
-    else
-    {
-    leitura[0]=0;
-    }
-    acaoA = estadoA;
-  }
     estadoAnteriorA = estadoA;
 
-  //? Botão B
-  bool estadoB = digitalRead(PIN_B);
-  static bool estadoAnteriorB = 1;
-  static unsigned int tempoAnteriorB;
-  static bool acaoB = 1;
-  if (estadoB != estadoAnteriorB && estadoB == 0)
-    tempoAnteriorB = tempo;
-  if (tempo - tempoAnteriorB > 100)
-  {
-    //Serial.println(acaoD);
-    if (acaoB != estadoB)
+    //? Botão B
+    bool estadoB = digitalRead(PIN_B);
+    static bool estadoAnteriorB = 1;
+    static unsigned int tempoAnteriorB;
+    static bool acaoB = 1;
+    if (estadoB != estadoAnteriorB && estadoB == 0)
+      tempoAnteriorB = tempo;
+    if (tempo - tempoAnteriorB > 100)
     {
-    if(estadoB == 0)
-    leitura[1] = 1;
+      if (acaoB != estadoB)
+      {
+        if (estadoB == 0)
+          leitura[1] = 1;
+      }
+      else
+      {
+        leitura[1] = 0;
+      }
+      acaoB = estadoB;
     }
-    else
-    {
-    leitura[1]=0;
-    }
-    acaoB = estadoB;
-  }
     estadoAnteriorB = estadoB;
 
-  //? Botão C
-  bool estadoC = digitalRead(PIN_C);
-  static bool estadoAnteriorC = 1;
-  static unsigned int tempoAnteriorC;
-  static bool acaoC = 1;
-  if (estadoC != estadoAnteriorC && estadoC == 0)
-    if (tempo - tempoAnteriorC > 100)
-  {
-    //Serial.println(acaoD);
-    if (acaoC != estadoC)
-    {
-    if(estadoC == 0)
-    leitura[2] = 1;
-    }
-    else
-    {
-    leitura[2]=0;
-    }
-    acaoC = estadoC;
-  }
+    //? Botão C
+    bool estadoC = digitalRead(PIN_C);
+    static bool estadoAnteriorC = 1;
+    static unsigned int tempoAnteriorC;
+    static bool acaoC = 1;
+    if (estadoC != estadoAnteriorC && estadoC == 0)
+        tempoAnteriorC = tempo;
+      if (tempo - tempoAnteriorC > 100)
+      {
+        if (acaoC != estadoC)
+        {
+          if (estadoC == 0)
+            leitura[2] = 1;
+        }
+        else
+        {
+          leitura[2] = 0;
+        }
+        acaoC = estadoC;
+      }
     estadoAnteriorC = estadoC;
 
-  //? Botão D
-  bool estadoD = digitalRead(PIN_D);
-  static bool estadoAnteriorD = 1;
-  static unsigned int tempoAnteriorD;
-  static bool acaoD =1;
-  if (estadoD != estadoAnteriorD)
-    tempoAnteriorD = tempo;
-  if (tempo - tempoAnteriorD > 100)
-  {
-    //Serial.println(acaoD);
-    if (acaoD != estadoD)
+    //? Botão D
+    bool estadoD = digitalRead(PIN_D);
+    static bool estadoAnteriorD = 1;
+    static unsigned int tempoAnteriorD;
+    static bool acaoD = 1;
+    if (estadoD != estadoAnteriorD)
+      tempoAnteriorD = tempo;
+    if (tempo - tempoAnteriorD > 100)
     {
-    if(estadoD == 0)
-    leitura[3] = 1;
+      if (acaoD != estadoD)
+      {
+        if (estadoD == 0)
+          leitura[3] = 1;
+      }
+      else
+      {
+        leitura[3] = 0;
+      }
+      acaoD = estadoD;
     }
-    else
-    {
-    leitura[3]=0;
-    }
-    acaoD = estadoD;
-  }
     estadoAnteriorD = estadoD;
 
-
-  //? Botão E
-  bool estadoE = digitalRead(PIN_E);
-  static bool estadoAnteriorE = 1;
-  static unsigned int tempoAnteriorE;
-  static bool acaoE =1;
-  if (estadoE != estadoAnteriorE )
-    tempoAnteriorE = tempo;
- if (tempo - tempoAnteriorE > 100)
-  {
-    //Serial.println(acaoD);
-    if (acaoE != estadoD)
+    //? Botão E
+    bool estadoE = digitalRead(PIN_E);
+    static bool estadoAnteriorE = 1;
+    static unsigned int tempoAnteriorE;
+    static bool acaoE = 1;
+    if (estadoE != estadoAnteriorE)
+      tempoAnteriorE = tempo;
+    if (tempo - tempoAnteriorE > 100)
     {
-    if(estadoE == 0)
-    leitura[4] = 1;
+      if (acaoE != estadoD)
+      {
+        if (estadoE == 0)
+          leitura[4] = 1;
+      }
+      else
+      {
+        leitura[4] = 0;
+      }
+      acaoE = estadoE;
     }
-    else
-    {
-    leitura[4]=0;
-    }
-    acaoE = estadoE;
-  }
     estadoAnteriorE = estadoE;
-}
+  }
 }
