9a931fd22cbdfaed0e49e95fc9510533bef7b69a
guirrs <guirsantos189@gmail.com>
2025-06-11 10:05:17 -0300

    Serial

--- Controle do jogo/src/main.cpp
+++ Controle do jogo/src/main.cpp
@@ -6,15 +6,15 @@ LiquidCrystal_I2C lcd(0x27, 20, 4);
 #define PIN_A 2
 #define PIN_B 4
 #define PIN_C 18
-#define PIN_D 5
+#define PIN_D 19
 #define PIN_E 25
 
 void leituraBotoes(bool leitura[5]);
-int selecaoSerial(bool botaoA, bool botaoB, bool botaoC, bool botaoD, bool botaoE);
-// Declaração da função que fará a leitura com debounce de todos os 5 botões
+int selecaoSerial(bool botaoA, bool botaoB, bool botaoC, bool botaoD);
+void ataques(int magia, bool botaoB, bool D);
 
 void setup()
-{ // Configura dos pinos como entrada com resistor interno pull-up
+{
   pinMode(PIN_A, INPUT_PULLUP);
   pinMode(PIN_B, INPUT_PULLUP);
   pinMode(PIN_C, INPUT_PULLUP);
@@ -31,37 +31,170 @@ void setup()
 
 void loop()
 {
-  bool botao[5];  // Cria um array de 5 valores booleanos para guardar o estado de cada botão
+  bool botao[5];
   static int golpes;
-  leituraBotoes(botao);   // Chama a função que faz a leitura de cada botão com debounce e preenche o array
+  leituraBotoes(botao);
 
+  bool A = botao[0];  if(A) Serial.println("A");//! sobe
+  bool B = botao[1];  if(B) Serial.println("B");//! voltar
+  bool C = botao[2];  if(C) Serial.println("C");//! descer
+  bool D = botao[3];  if(D) Serial.println("D");//! avancar
+  bool E = botao[4];  if(E) Serial.println("E");//* inderterminado
 
-  bool A = botao[0];  if(A) Serial.println("A");
-  bool B = botao[1];  if(B) Serial.println("B");
-  bool C = botao[2];  if(C) Serial.println("C");
-  bool D = botao[3];  if(D) Serial.println("D");
-  bool E = botao[4];  if(E) Serial.println("E");
-  golpes = selecaoSerial(A, B, C, D, E);
+  //* As magias e a selecao sao dividas de 0 a 3
+  golpes = selecaoSerial(A, B, C, D);
+  ataques(golpes, B, D);
 }
 
-int selecaoSerial(bool botaoA, bool botaoB, bool botaoC, bool botaoD, bool botaoE)
+
+
+void leituraBotoes(bool leitura[5])
+{
+  unsigned int tempo = millis();
+  static bool verdadeiro = 1;
+
+  if (tempo > 100)
+  {
+    //? Botão A
+    bool estadoA = digitalRead(PIN_A);
+    static bool estadoAnteriorA = 1;
+    static unsigned int tempoAnteriorA;
+    static bool acaoA = 1;
+
+    if (estadoA != estadoAnteriorA && estadoA == 0)
+      tempoAnteriorA = tempo;
+    if (tempo - tempoAnteriorA > 100)
+    {
+      if (acaoA != estadoA)
+      {
+        if (estadoA == 0)
+          leitura[0] = 1;
+      }
+      else
+      {
+        leitura[0] = 0;
+      }
+      acaoA = estadoA;
+    }
+    estadoAnteriorA = estadoA;
+
+    //? Botão B
+    bool estadoB = digitalRead(PIN_B);
+    static bool estadoAnteriorB = 1;
+    static unsigned int tempoAnteriorB;
+    static bool acaoB = 1;
+    if (estadoB != estadoAnteriorB && estadoB == 0)
+      tempoAnteriorB = tempo;
+    if (tempo - tempoAnteriorB > 100)
+    {
+      if (acaoB != estadoB)
+      {
+        if (estadoB == 0)
+          leitura[1] = 1;
+      }
+      else
+      {
+        leitura[1] = 0;
+      }
+      acaoB = estadoB;
+    }
+    estadoAnteriorB = estadoB;
+
+    //? Botão C
+    bool estadoC = digitalRead(PIN_C);
+    static bool estadoAnteriorC = 1;
+    static unsigned int tempoAnteriorC;
+    static bool acaoC = 1;
+    if (estadoC != estadoAnteriorC && estadoC == 0)
+        tempoAnteriorC = tempo;
+      if (tempo - tempoAnteriorC > 100)
+      {
+        if (acaoC != estadoC)
+        {
+          if (estadoC == 0)
+            leitura[2] = 1;
+        }
+        else
+        {
+          leitura[2] = 0;
+        }
+        acaoC = estadoC;
+      }
+    estadoAnteriorC = estadoC;
+
+    //? Botão D
+    bool estadoD = digitalRead(PIN_D);
+    static bool estadoAnteriorD = 1;
+    static unsigned int tempoAnteriorD;
+    static bool acaoD = 1;
+    if (estadoD != estadoAnteriorD)
+      tempoAnteriorD = tempo;
+    if (tempo - tempoAnteriorD > 100)
+    {
+      if (acaoD != estadoD)
+      {
+        if (estadoD == 0)
+          leitura[3] = 1;
+      }
+      else
+      {
+        leitura[3] = 0;
+      }
+      acaoD = estadoD;
+    }
+    estadoAnteriorD = estadoD;
+
+    //? Botão E
+    bool estadoE = digitalRead(PIN_E);
+    static bool estadoAnteriorE = 1;
+    static unsigned int tempoAnteriorE;
+    static bool acaoE = 1;
+    if (estadoE != estadoAnteriorE)
+      tempoAnteriorE = tempo;
+    if (tempo - tempoAnteriorE > 100)
+    {
+      if (acaoE != estadoD)
+      {
+        if (estadoE == 0)
+          leitura[4] = 1;
+      }
+      else
+      {
+        leitura[4] = 0;
+      }
+      acaoE = estadoE;
+    }
+    estadoAnteriorE = estadoE;
+  }
+}
+
+int selecaoSerial(bool botaoA, bool botaoB, bool botaoC, bool botaoD)
 {
   //? Selecao
   static int selecao;
   static int selecaoAnterior;
+  static bool agir;
 
+  if(botaoD)
+  agir = true;
+  if(botaoB)
+  agir = false;
+
+  if(!agir)
+{
+  lcd.clear();
   if (botaoA)
     selecao--;
   if (botaoC)
     selecao++;
-Serial.println(selecao);
+
   if (selecao > 3)
     selecao = 0;
   if (selecao < 0)
     selecao = 3;
 
     //* Impedir que pule 3 a 1
-    if(selecao == 3 && selecaoAnterior == 1)
+    if(selecao == 1 && selecaoAnterior == 3)
     selecao = 2;
     
     selecaoAnterior = selecao;
@@ -102,126 +235,55 @@ Serial.println(selecao);
     lcd.setCursor(0, 0);
     lcd.print(" ");
   }
+}
   return selecao;
 }
 
-// Função para ler cada botão com debouncing
-void leituraBotoes(bool leitura[5])
+void ataques(int magia, bool botaoB, bool botaoD)
 {
-  unsigned int tempo = millis();
-  static bool verdadeiro = 1;
-
-  if (tempo > 100)
-  { // Só começa a processar depois dos primeiros 100 ms de boot, para dar tempo ao sistema
-    //? ============LÊ BOTÃO A=============== 
-    bool estadoA = digitalRead(PIN_A);
-    static bool estadoAnteriorA = 1;
-    static unsigned int tempoAnteriorA;
-    static bool acaoA = 1;
-
-    if (estadoA != estadoAnteriorA && estadoA == 0)
-      tempoAnteriorA = tempo;
-    if (tempo - tempoAnteriorA > 100)
+  if(magia == 0)
+  {
+    if(botaoD == 1)
     {
-      if (acaoA != estadoA)
-      {
-        if (estadoA == 0)
-          leitura[0] = 1;
-      }
-      else
-      {
-        leitura[0] = 0;
-      }
-      acaoA = estadoA;
+    Serial.println("Magia 1");//*golpe
+    lcd.clear();
     }
-    estadoAnteriorA = estadoA;
-
-    //? ============LÊ BOTÃO B=====================
-    bool estadoB = digitalRead(PIN_B);
-    static bool estadoAnteriorB = 1;
-    static unsigned int tempoAnteriorB;
-    static bool acaoB = 1;
-    if (estadoB != estadoAnteriorB && estadoB == 0)
-      tempoAnteriorB = tempo;
-    if (tempo - tempoAnteriorB > 100)
+    if(botaoB);
+    //volta a selecao
+  }
+  if(magia == 1)
+  {
+    if(botaoD)
     {
-      if (acaoB != estadoB)
-      {
-        if (estadoB == 0)
-          leitura[1] = 1;
-      }
-      else
-      {
-        leitura[1] = 0;
-      }
-      acaoB = estadoB;
+       Serial.println("Magia 2");//*golpe
+       lcd.clear();
     }
-    estadoAnteriorB = estadoB;
 
-    //? ============LÊ BOTÃO C=====================
-    bool estadoC = digitalRead(PIN_C);
-    static bool estadoAnteriorC = 1;
-    static unsigned int tempoAnteriorC;
-    static bool acaoC = 1;
-    if (estadoC != estadoAnteriorC && estadoC == 0)
-        tempoAnteriorC = tempo;
-      if (tempo - tempoAnteriorC > 100)
-      {
-        if (acaoC != estadoC)
-        {
-          if (estadoC == 0)
-            leitura[2] = 1;
-        }
-        else
-        {
-          leitura[2] = 0;
-        }
-        acaoC = estadoC;
-      }
-    estadoAnteriorC = estadoC;
-
-    //? ============LÊ BOTÃO D=====================
-    bool estadoD = digitalRead(PIN_D);
-    static bool estadoAnteriorD = 1;
-    static unsigned int tempoAnteriorD;
-    static bool acaoD = 1;
-    if (estadoD != estadoAnteriorD)
-      tempoAnteriorD = tempo;
-    if (tempo - tempoAnteriorD > 100)
+    if(botaoB);
+    //volta a selecao
+  }
+  if(magia == 2)
+  {
+    if(botaoD)
     {
-      if (acaoD != estadoD)
-      {
-        if (estadoD == 0)
-          leitura[3] = 1;
-      }
-      else
-      {
-        leitura[3] = 0;
-      }
-      acaoD = estadoD;
+       Serial.println("Magia 3");//*golpe
+       lcd.clear();
     }
-    estadoAnteriorD = estadoD;
 
-    //? ============LÊ BOTÃO E=====================
-    bool estadoE = digitalRead(PIN_E);
-    static bool estadoAnteriorE = 1;
-    static unsigned int tempoAnteriorE;
-    static bool acaoE = 1;
-    if (estadoE != estadoAnteriorE)
-      tempoAnteriorE = tempo;
-    if (tempo - tempoAnteriorE > 100)
+    if(botaoB);
+    //volta a selecao
+  }
+  if(magia == 3)
+  {
+    if(botaoD)
     {
-      if (acaoE != estadoD)
-      {
-        if (estadoE == 0)
-          leitura[4] = 1;
-      }
-      else
-      {
-        leitura[4] = 0;
-      }
-      acaoE = estadoE;
+       Serial.println("Magia 4");//*golpe
+       lcd.clear();
     }
-    estadoAnteriorE = estadoE;
+
+    if(botaoB);
+    //volta a selecao
   }
 }
+
+
