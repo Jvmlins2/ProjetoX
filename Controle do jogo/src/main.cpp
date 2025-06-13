bfe3886ad7c2188201fe6036f912ac2d1ee76437
GUILHERME RIBEIRO DOS SANTOS <46336142825@info.local>
2025-06-13 08:59:47 -0300

    Atualizacao2.0

--- Controle do jogo/src/main.cpp
+++ Controle do jogo/src/main.cpp
@@ -1,5 +1,23 @@
 #include <Arduino.h>
 #include <LiquidCrystal_I2C.h>
+#include <WiFi.h>
+#include "internet.h"
+#include <ArduinoJson.h>
+#include <PubSubClient.h>
+
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+const char *mqtt_server = "broker.hivemq.com";
+const int mqtt_port = 1883;
+const char *mqtt_id = "esp32-senai134-magicduelcontrole1";
+const char *mqtt_topic_sub = "senai134/magicduel/console";
+const char *mqtt_topic_pub = "senai134/magicduel/controles";
+
+void callback(char *, byte *, unsigned int);
+void mqttConnect(void);
+
 
 LiquidCrystal_I2C lcd(0x27, 20, 4);
 
@@ -27,10 +45,23 @@ void setup()
   lcd.backlight();
   lcd.home();
   lcd.print(">");
+
+  conectaWiFi();
+
+  client.setServer(mqtt_server, mqtt_port);
+  client.setCallback(callback);
 }
 
 void loop()
 {
+  heckWiFi();
+
+  if (!client.connected())
+    mqttConnect();
+
+  client.loop();
+
+
   bool botao[5];
   static int golpes;
   leituraBotoes(botao);
@@ -285,5 +316,39 @@ void ataques(int magia, bool botaoB, bool botaoD)
     //volta a selecao
   }
 }
+//*===========================================================================================================
 
+void callback(char *topic, byte *payload, unsigned int length)
+{
+  Serial.printf("mensagem recebida em %s: ", topic);
 
+  String mensagem = "";
+  for (unsigned int i = 0; i < length; i++)
+  {
+    char c = (char)payload[i];
+    mensagem += c;
+  }
+  Serial.println(mensagem);
+}
+
+void mqttConnect()
+{
+  while (!client.connected())
+  {
+    Serial.println("Conectando ao MQTT...");
+
+    if (client.connect(mqtt_id))
+    {
+      Serial.println("Conectado com sucesso");
+      client.subscribe(mqtt_topic_sub);
+    }
+
+    else
+    {
+      Serial.print("falha, rc=");
+      Serial.println(client.state());
+      Serial.println("tentando novamente em 5 segundos");
+      delay(5000);
+    }
+  }
+}
